0b5c19484f49662739f7bb187c47bcb05735131c
jannatpakhi927-create <jannatpakhi927@gmail.com>
2026-06-21 09:46:53 +0000

    Datatype_summary

--- 01_basics/Datatype_summary.js
+++ 01_basics/Datatype_summary.js
@@ -5,7 +5,7 @@ let age = 25;
 let pi = 3.14;
 
 let isLoggedIn = true;
-let x;
+let hello;
 let user = null;
 
 const id1 = Symbol("123");
@@ -26,4 +26,55 @@ const myFunction = function(){
   console.log("Hi");
 }
 
-console.log(typeof x);
+console.log(typeof hello);
+
+
+
+
+
+// ********************** Class 10: Stack and Heap memory  *************************
+// Stack(Primitive), Heap(Non-Primitive)
+
+let youtubeName = "nahu"
+let anothername = youtubeName
+anothername = "helloworld"
+
+console.log(anothername);
+console.log(youtubeName);
+
+// Rules: Primitive Type ⇒ Copy তৈরি হয়, {Copy Change করলে Original Change হবে না}
+
+let userOne = {
+    email : "test123@google.com",
+    upi : "test@123"
+}
+
+let userTwo = userOne
+userTwo.email = "hello@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// Rules: Non-Primitive Type => Reference Copy হয়, {Reference Change করলে Original Object-ও Change হয়}
+
+// Additional Practice: 
+let user1 = {
+  name: "Jannat"
+};
+
+let user2 = user1;
+
+user2 = {
+  name: "Pakhi"
+};
+
+console.log(user1.name);
+console.log(user2.name);
+
+/*
+মনে রাখার Shortcut
+1. Primitive --> Stack --> Copy --> Original Safe
+
+2. Object / Array / Function --> Heap --> Reference -->Original Can Change
+*/
+
